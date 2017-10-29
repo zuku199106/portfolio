ae5c30160708603c8c7a53327027ae2ddc9c765d
Tomasz Zukowski <tomasz.zukowski91@gmail.com>
2017-10-29 17:14:24 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Tomasz Żukowski - Frontend/Backend Developer</title>
+    <link rel="stylesheet" href="style/style.css">
+  </head>
+  <body>
+  
+  </body>
+</html>
